792c3b8d3326afc55fc8b3efd9471abae5f0d40d
ckimani <ckimani32@gmail.com>
2017-01-17 18:16:55 +0300

    Create PrimeFunction.py

---
+++ PrimeFunction.py
@@ -0,0 +1,8 @@
+import unittest
+#Importing the function into the test file.
+from PrimeFunctionProgram import printPrimes
+class PrimeCheck(unittest.TestCase)
+    def test_number_not_negative(self):
+    	#This test checks to see whether the number is a positive number
+    	self.assertFalse(is_prime(index))
+
