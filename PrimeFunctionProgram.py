83102570134eb9873e1b43404727135ff9a193da
ckimani <ckimani32@gmail.com>
2017-01-18 14:21:57 +0300

    Update PrimeFunctionProgram.py

--- PrimeFunctionProgram.py
+++ PrimeFunctionProgram.py
@@ -3,17 +3,17 @@ def prime(n):
 	This function will seek to find out if a list of numbers,
 	from 0 to n, has prime numbers.
 	"""
-    listx = list()_   
+     primeList=list()  
     for num in range(2, n+1):
         prime = True
         for i in range(2, num):
             if (num % i == 0):
                 prime = False
         if prime:
-            listx.append(num)
+            primelist.append(num)
         else:
         	return 'Not prime'
-    return listx
+    return primelist
 
 
 print prime (10)
