0756c1d838c9fe222b95fc02e88734b82ced2575
ckimani <ckimani32@gmail.com>
2017-01-21 15:49:48 +0300

    Update PrimeFunction.py

--- PrimeFunction.py
+++ PrimeFunction.py
@@ -5,4 +5,6 @@ class PrimeCheck(unittest.TestCase)
     def test_number_not_negative(self):
     	#This test checks to see whether the number is a positive number
     	self.assertFalse(is_prime(index))
-
+'''
+Implementation of more test cases
+'''
